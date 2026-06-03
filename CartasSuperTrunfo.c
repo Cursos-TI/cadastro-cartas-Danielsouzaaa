2063802ed632d9b4f77deb6e565db9222c92a432
Danielsouzaaa <daniel_msouza20@hotmail.com>
2026-06-03 14:55:53 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,5 +11,39 @@ int main() {
 
   // Área para exibição dos dados da cidade
 
+  // Variáveis da Carta 1
+    char estado1;
+    char codigo1[];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+
+   // Cadastro da Carta 1
+    printf("=== Cadastro da Carta 1 ===\n");
+
+    printf("Estado (): ");
+    scanf(" %c", &estado1);
+
+    printf("Codigo da Carta: ");
+    scanf("%s", codigo1);
+
+    printf("Nome da Cidade: ");
+    scanf(" %s\n", cidade1);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao1);
+
+    printf("Area (km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB (bilhoes): ");
+    scanf("%f", &pib1);
+
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos1);
+  
+
 return 0;
 } 
